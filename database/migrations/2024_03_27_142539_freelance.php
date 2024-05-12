2f3b84919e581eef4714f3c8e3c8aa0e845008bb
marine-rle <120376794+marine-rle@users.noreply.github.com>
2024-05-12 19:13:16 +0200

    update migration freelance

--- database/migrations/2024_03_27_142539_freelance.php
+++ database/migrations/2024_03_27_142539_freelance.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('freelances', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('description');
             $table->timestamps();
         });
